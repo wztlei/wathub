2b7e8d517362fb00da514450e0fae307cf1bb565
wztlei <william.zt.lei@gmail.com>
2019-06-30 15:35:00 -0400

    Reformat some files

--- app/src/main/java/io/github/wztlei/wathub/net/Contract.java
+++ app/src/main/java/io/github/wztlei/wathub/net/Contract.java
@@ -2,6 +2,7 @@ package io.github.wztlei.wathub.net;
 
 import io.github.wztlei.wathub.model.PhotoDetails;
 import io.github.wztlei.wathub.model.PhotoSize;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
@@ -9,27 +10,27 @@ import java.util.List;
 
 public interface Contract {
 
-  class Size {
-    @SerializedName("sizes")
-    SizeInfo mSize;
+    class Size {
+        @SerializedName("sizes")
+        SizeInfo mSize;
 
-    public List<PhotoSize> getSizes() {
-      return (mSize == null) ? new ArrayList<>() : mSize.mSizes;
+        public List<PhotoSize> getSizes() {
+            return (mSize == null) ? new ArrayList<>() : mSize.mSizes;
+        }
     }
-  }
 
-  class SizeInfo {
-    @SerializedName("size")
-    List<PhotoSize> mSizes;
-  }
-
-  class Photo {
-    @SerializedName("photo")
-    PhotoDetails mPhoto;
-
-    public PhotoDetails getDetails() {
-      return mPhoto;
+    class SizeInfo {
+        @SerializedName("size")
+        List<PhotoSize> mSizes;
     }
-  }
+
+    class Photo {
+        @SerializedName("photo")
+        PhotoDetails mPhoto;
+
+        public PhotoDetails getDetails() {
+            return mPhoto;
+        }
+    }
 
 }
