196ecf15c3306e735632a2209bda12a8b061b725
Rolf Li <rolf.x.li@gmail.com>
2019-07-14 14:45:04 -0400

    sendout is working not crashing issue: not scrolling as intended

--- app/src/main/java/io/github/wztlei/wathub/ui/modules/Feedback/feedbacksendout.java
+++ app/src/main/java/io/github/wztlei/wathub/ui/modules/Feedback/feedbacksendout.java
@@ -6,7 +6,14 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface feedbacksendout {
-    @POST("1FAIpQLSc3qx_rg_v6uZbzX6c0tw8uhOpsCRgAfH2-FiB1j0hLbIw_mA/viewform")
+    /* for future reference (in case of forum change)
+    *  after google forum creation, click preview
+    *  inspect page url, we find 3 'entry.' fields for the 3 inputs
+    * find 'formResponse' to find the input URL
+    * in this first case: https://docs.google.com/forms/d/e/1FAIpQLSc3qx_rg_v6uZbzX6c0tw8uhOpsCRgAfH2-FiB1j0hLbIw_mA/formResponse
+    * remove everything up to and including e/
+    */
+    @POST("1FAIpQLSc3qx_rg_v6uZbzX6c0tw8uhOpsCRgAfH2-FiB1j0hLbIw_mA/formResponse")
     @FormUrlEncoded
     Call<Void> fbSend(
             @Field("entry.1283738959") String fb,
