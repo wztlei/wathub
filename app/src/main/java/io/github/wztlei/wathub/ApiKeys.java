1d25244b09cbb615c5d7777642b5b8f5bfe4a6bf
wztlei <william.zt.lei@gmail.com>
2019-08-31 23:12:44 -0400

    WL - Remove ApiKeys.java

--- app/src/main/java/io/github/wztlei/wathub/ApiKeys.java
+++
@@ -1,7 +0,0 @@
-package io.github.wztlei.wathub;
-
-public class ApiKeys {
-    public static final String FLICKR_API_KEY = "";
-    public static final String UWATERLOO_API_KEY = "PLACE YOUR UWATERLOO API KEY HERE";
-    static final String APPLICATION_ID = "io.github.wztlei.wathub";
-}
